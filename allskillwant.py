8b95aea915d8083a2b7d402b225deb194bca9a0c
mai <61070214@kmitl.ac.th>
2018-11-22 00:41:17 +0700

    Test
    กราฟ

---
+++ allskillwant.py
@@ -0,0 +1,28 @@
+import os
+import csv
+import pygal
+lis = []
+usecal = 5
+def openfile():
+    with open('csvfile/job.csv') as csvfile:
+        file1 = csv.reader(csvfile)
+        print('open Success')
+        usefile(list(file1))
+def usefile(file1):
+    data1 = []
+    xlabel = [i[0] for i in file1[1:]]
+    for i in range(1, 5):
+        head = file1[0][i]
+        lisskill = [row[i] for row in file1[1:]]
+        summ = list(map(lambda x: int(x.replace(',','')), lisskill))
+        data1.append([head, summ])
+    chart(data1, xlabel)
+def chart(data, label):
+    want = 10
+    line_chart = pygal.StackedBar()
+    line_chart.title = 'skill want top 10'
+    line_chart.x_labels = label[:want]
+    for i, j in data:
+        line_chart.add(i, j[:want])
+    line_chart.render_to_file('img/allskillwant2.svg')
+openfile()
